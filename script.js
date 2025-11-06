709c640d8124ae37a2349324cc13696033849a52
mahbub-sakib <mahbub.sakib418@gmail.com>
2025-11-06 20:33:00 +0600

    remove btn logic added

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+'use strict';
+
 const toggleBtn = document.querySelector('#theme-toggle-btn');
 const body = document.body;
 const taskList = document.getElementById('dynamic-section');
 const addCardBtn = document.getElementById('add-card-btn');
 const noTaskText = document.getElementById('no-task-text');
+
+let first_cross_displayed = false;
 
 // Theme Toggle
 toggleBtn.addEventListener('click', () => {
@@ -38,16 +42,47 @@ function createTask() {
     });
 
     task.appendChild(input);
-    task.appendChild(removeBtn);
+
+    const total_cards = document.getElementsByClassName("card").length;
+
+
+    if (total_cards !== 0) {
+        task.appendChild(removeBtn);
+    }
+
     taskList.appendChild(task);
+
+    if (total_cards > 0 && first_cross_displayed == false) {
+        // console.log("dhukse");
+        const first_card = document.querySelector(".card");
+
+        const xBtn = document.createElement('button');
+        xBtn.classList.add('remove-card-btn');
+        xBtn.textContent = "×";
+
+        xBtn.addEventListener('click', () => {
+            task.remove();
+            updateNoTaskText();
+        });
+
+        first_card.appendChild(xBtn);
+
+        first_cross_displayed = true;
+    }
 
     updateNoTaskText();
 }
 
 // Create initial task
 createTask();
+
 updateNoTaskText();
+
+
 
 // Add new task button
 addCardBtn.addEventListener('click', createTask);
 
+document.addEventListener('keydown', function (e) {
+    console.log(e.key);
+})
